650330cce748428747edce9ce57d0e18e1da96f6
Jean-Denis VIDOT <5817251+jh3ady@users.noreply.github.com>
2025-04-10 21:50:06 +0200

    :art: chore(phpstan): Resolve issues

--- apps/web/symfony/config/preload.php
+++ apps/web/symfony/config/preload.php
@@ -1,5 +1,7 @@
 <?php
 
+use function Safe\opcache_compile_file;
+
 if (file_exists(dirname(__DIR__) . '/var/cache/prod/App_KernelProdContainer.preload.php')) {
     opcache_compile_file(dirname(__DIR__) . '/var/cache/prod/App_KernelProdContainer.preload.php');
 }
--- apps/web/symfony/public/index.php
+++ apps/web/symfony/public/index.php
@@ -4,6 +4,10 @@ use SymfonyCleanBoilerplate\Web\App\Kernel;
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
-return static function (array $context) {
+return static function (array $context): Kernel {
+    if (!isset($context['APP_ENV']) || !is_string($context['APP_ENV'])) {
+        throw new LogicException('The APP_ENV environment variable must be set.');
+    }
+
     return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
 };
